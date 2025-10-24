5bd8334f8d13e27ff82a8e40575b77f4aa28bff8
vivian <vivianl@mega7.fun>
2025-10-25 00:03:44 +0800

    feat: add share lib functions and QrCode image

---
+++ lib/site.ts
@@ -0,0 +1,9 @@
+export const SITE = {
+  storeName: '羊肉盧',
+  address: '970 花蓮縣花蓮市府前路396號',
+  phone: '0982-724358',
+  lineQrCode: '/lineQr.png',
+  IGQrCode: '/IGQr.png',
+  MetaQrCode: '/metaQr.png',
+  GoogleMapQrCode: '/googleMapQr.png',
+}
